ca907f8717f964546912b71ef836a8e86fca55be
Ritika Chadha <ritikachadha05@gmail.com>
2018-03-07 13:02:12 -0800

    Adding Errors

--- ticketviewer/routes/index.js
+++ ticketviewer/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
 
   var options = {
@@ -16,20 +15,20 @@ router.get('/', function(req, res, next) {
 
   function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body.tickets);
       res.render('index',
       { title: 'Ticket Viewer',
         tickets: body.tickets
       });
     }
+
+    else {
+      res.render('error',{
+        title: 'Error Page',
+        error: 'Page not Found'
+      })
+    }
   }
   request.get(options, callback);
-
-});
-
-router.get('/alltickets', function (req, res) {
-
-
 
 });
 
@@ -45,9 +44,27 @@ var options = {
 };
 
 function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
+  if (!body.error && !response.statusCode == 404) {
+    res.render('index',
+    { title: 'Ticket Viewer',
+      tickets: body
+    });
+  }  else if (body.error.title){
+    res.render('error',
+    {
+      title: 'Page Error',
+      error: body.error.title,
+      message: body.error.message
+    });
   }
+  else{
+    res.render('error',
+    {
+      title: 'Page Error',
+      error: body.error
+    });
+  }
+
 }
 request.get(options, callback);
 
@@ -68,16 +85,16 @@ request.get(options, callback);
 // }).pipe(res);
 // });
 
-router.get('/singleticket/:id', function (req, res) {
-  request({
-  uri: 'https://ritikachadha.zendesk.com/api/v2/tickets/'+id+'.json',
-  auth:{
-    user: 'ritikachadha05@gmail.com',
-    pass: 'qwerty'
-  },
-  type: 'GET',
-  dataType: 'json'
-}).pipe(res);
-});
+// router.get('/singleticket/:id', function (req, res) {
+//   request({
+//   uri: 'https://ritikachadha.zendesk.com/api/v2/tickets/'+id+'.json',
+//   auth:{
+//     user: 'ritikachadha05@gmail.com',
+//     pass: 'qwerty'
+//   },
+//   type: 'GET',
+//   dataType: 'json'
+// }).pipe(res);
+// });
 
 module.exports = router;
